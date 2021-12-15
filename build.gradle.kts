152434a3815e8c2f2660c2e22eeeba9942ad8574
morgan <morgan@dunamu.com>
2021-12-15 19:04:41 +0900

    added compilable proto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,22 @@
+import com.google.protobuf.gradle.*
+
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://plugins.gradle.org/m2/")
+            mavenCentral()
+        }
+    }
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
+    }
+}
+
 plugins {
+    idea
+    java
     kotlin("jvm") version "1.6.10"
+    id("com.google.protobuf") version "0.8.18"
 }
 
 group = "com.sample"
@@ -9,8 +26,51 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/java/")
+            srcDirs("build/generated/source/proto/main/grpc/")
+        }
+        proto {
+            srcDirs("src/main/proto/")
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.1"
+    }
+    plugins {
+        id("grpc"){
+            artifact = "io.grpc:protoc-gen-grpc-java:1.42.1"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach { task ->
+            task.builtins {
+                java
+            }
+            task.plugins {
+                id("grpc")
+            }
+        }
+    }
+}
+
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("com.google.api.grpc:proto-google-common-protos:2.7.0")
     implementation("io.grpc:grpc-protobuf:1.42.1")
+
     implementation("org.apache.tomcat:tomcat-annotations-api:10.0.14")
+}
+
+tasks {
+    withType<Copy> {
+        filesMatching("**/*.proto") {
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
 }
