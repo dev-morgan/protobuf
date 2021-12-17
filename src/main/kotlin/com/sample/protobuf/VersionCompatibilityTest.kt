2eac1f2bc2484825ab4b20d13993bc32ba82b1b2
morgan <morgan@dunamu.com>
2021-12-17 19:00:10 +0900

    added reserved example

---
+++ src/main/kotlin/com/sample/protobuf/VersionCompatibilityTest.kt
@@ -0,0 +1,32 @@
+package com.sample.protobuf
+
+import com.sample.models.Television
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+//    val television = Television.newBuilder()
+//        .setBrand("sony")
+//        .setYear(2015)
+//        .build()
+//
+//    val pathV1 = Paths.get("tv-v1")
+//    Files.write(pathV1, television.toByteArray())
+
+//    val television = Television.newBuilder()
+//        .setBrand("sony")
+//        .setModel(2016)
+//        .setType(Type.OLED)
+//        .build()
+
+//    val pathV2 = Paths.get("tv-1")
+//    Files.write(pathV2, television.toByteArray())
+
+    val pathV1 = Paths.get("tv-v1")
+    val bytes = Files.readAllBytes(pathV1)
+
+    println(
+        Television.parseFrom(bytes).price
+    )
+
+}
