c8ba101c7c7c8eb3c959f20804f9f2aa47104f25
morgan <morgan@dunamu.com>
2021-12-17 17:15:34 +0900

    inline assemble function

--- src/main/kotlin/com/sample/protobuf/CompositionDemo.kt
+++ src/main/kotlin/com/sample/protobuf/CompositionDemo.kt
@@ -5,43 +5,31 @@ import com.sample.models.Car
 import com.sample.models.Person
 import mu.KLogging
 
-class CompositionDemo {
-    companion object : KLogging()
-
-    lateinit var person: Person
-
-    fun assemble(): CompositionDemo {
-        val address = Address.newBuilder()
-            .setPostbox(1234)
-            .setStreet("kang-nam")
-            .setCity("seoul")
-            .build()
-
-        val modelX = Car.newBuilder()
-            .setMake("tesla")
-            .setModel("Model X")
-            .setYear(2021)
-            .build()
-
-        val model3 = Car.newBuilder()
-            .setMake("tesla")
-            .setModel("Model 3")
-            .setYear(2020)
-            .build()
-
-        val person = Person.newBuilder()
-            .setName("morgan")
-            .setAge(17)
-            .setAddress(address)
-            .addAllCar(listOf(modelX, model3))
-            .build()
-
-        this.person = person
-        return this
-    }
-}
-
 fun main() {
-    val demo = CompositionDemo().assemble()
-    println(demo.person)
+    val address = Address.newBuilder()
+        .setPostbox(1234)
+        .setStreet("kang-nam")
+        .setCity("seoul")
+        .build()
+
+    val modelX = Car.newBuilder()
+        .setMake("tesla")
+        .setModel("Model X")
+        .setYear(2021)
+        .build()
+
+    val model3 = Car.newBuilder()
+        .setMake("tesla")
+        .setModel("Model 3")
+        .setYear(2020)
+        .build()
+
+    val person = Person.newBuilder()
+        .setName("morgan")
+        .setAge(17)
+        .setAddress(address)
+        .addAllCar(listOf(modelX, model3))
+        .build()
+
+    println(person)
 }
--- src/main/kotlin/com/sample/protobuf/MapDemo.kt
+++ src/main/kotlin/com/sample/protobuf/MapDemo.kt
@@ -4,37 +4,26 @@ import com.sample.models.BodyStyle
 import com.sample.models.Car
 import com.sample.models.Dealer
 
-class MapDemo {
-    lateinit var dealer: Dealer
-
-    fun assemble(): MapDemo {
-        val model3 = Car.newBuilder()
-            .setMake("tesla")
-            .setModel("Model 3")
-            .setBodyStyle(BodyStyle.SEDAN)
-            .setYear(2020)
-            .build()
-
-        val modelX = Car.newBuilder()
-            .setMake("tesla")
-            .setModel("Model X")
-            .setBodyStyle(BodyStyle.SUV)
-            .setYear(2021)
-            .build()
-
-        val dealer = Dealer.newBuilder()
-            .putAllModel(mapOf(2020 to model3, 2021 to modelX))
-            .build()
-        this.dealer = dealer
-        return this
-    }
-}
-
 fun main() {
-    val map = MapDemo().assemble()
-    println(map.dealer.getModelOrThrow(2020).bodyStyle)
+    val model3 = Car.newBuilder()
+        .setMake("tesla")
+        .setModel("Model 3")
+        .setBodyStyle(BodyStyle.SEDAN)
+        .setYear(2020)
+        .build()
+    val modelX = Car.newBuilder()
+        .setMake("tesla")
+        .setModel("Model X")
+        .setBodyStyle(BodyStyle.SUV)
+        .setYear(2021)
+        .build()
+    val dealer = Dealer.newBuilder()
+        .putAllModel(mapOf(2020 to model3, 2021 to modelX))
+        .build()
+
+    println(dealer.getModelOrThrow(2020).bodyStyle)
     println("------")
-    println(map.dealer.modelMap)
+    println(dealer.modelMap)
     println("------")
-    println(map.dealer.modelCount)
+    println(dealer.modelCount)
 }
